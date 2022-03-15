66291e5ca3fcf8758a7785b6fbe69865637514fd
naoki.sils <silsvlog@gmail.com>
2022-03-15 01:15:12 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,18 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build
-      @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+      @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
     end  
   end
+   
+  def create
+    @micropost= current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success]= "メッセージを投稿しました。"
+      redirect_to root_url
+    else
+      flash.now[:danger]= "メッセージの投稿に失敗しました。"
+      render "toppages/index"
+    end 
+  end  
 end
